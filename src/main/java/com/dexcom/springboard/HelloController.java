9753e5c0656cc176b0afdf502421937cebf783d0
Ajar Vashisth <ajar.vashisth@dexcom.com>
2019-05-29 21:10:02 -0700

    static page

--- src/main/java/com/dexcom/springboard/HelloController.java
+++ src/main/java/com/dexcom/springboard/HelloController.java
@@ -3,6 +3,7 @@ package com.dexcom.springboard;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -13,6 +14,10 @@ public class HelloController {
             defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
+    }
+    @GetMapping("/")
+    public String one() {
+        return "redirect:/static/index.html";
     }
 }
 
