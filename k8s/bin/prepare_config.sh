c23e6f4db03ca2b35c2ec8d1aec9444d91898717
Ajar Vashisth <ajar.vashisth@dexcom.com>
2019-05-30 17:38:19 -0700

    ci template

--- k8s/bin/prepare_config.sh
+++ k8s/bin/prepare_config.sh
@@ -15,7 +15,10 @@ export BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." >/dev/null 2>&1 
 
 export KUBE_NAMESPACE=germany
 export KUBE_APP_NAME=spring-ajar
+export KUBE_CLUSTER=kubernetes-p8-integrations-eu-dev-2
+export KUBE_REGION=europe-west3
 
+export DOLLAR='$'
 pushd ${BASE_DIR}
 
 echo .
@@ -28,6 +31,61 @@ for f in ${BASE_DIR}/k8s/templates/*.yaml ;
     echo "Processing $f -> ${BASE_DIR}/k8s/deploy/${f##*/} ";
 done
 
-popd
 
+cat << EOF > ${BASE_DIR}/src/main/resources/application.yaml
+#Info endpoints
+info:
+  company: Dexcom
+  project: integrations
+
+# Management endpoints
+management:
+  endpoint:
+    restart:
+      enabled:  true
+
+spring:
+  application:
+    name: ${KUBE_APP_NAME}
+  cloud:
+    kubernetes:
+      config:
+        name: ${KUBE_APP_NAME}
+        namespace: ${KUBE_NAMESPACE}
+        sources:
+          - name: ${KUBE_APP_NAME}
+      reload:
+        enabled: true
+        mode: polling
+        period: 2000
+
+EOF
+
+
+cat << EOF > ${BASE_DIR}/cloudbuild.yaml
+substitutions:
+  _DEXCOM_PROJECT_ID: ${KUBE_APP_NAME}
+  _DEXCOM_NAMESPACE: ${KUBE_NAMESPACE}
+images:
+  - "gcr.io/${DOLLAR}PROJECT_ID/${DOLLAR}{_DEXCOM_PROJECT_ID}:${DOLLAR}REVISION_ID"
+steps:
+  - name: 'gcr.io/cloud-builders/mvn'
+    args: ['install']
+  - name: 'gcr.io/cloud-builders/docker'
+    args: ['build', '--tag=gcr.io/${DOLLAR}PROJECT_ID/${DOLLAR}{_DEXCOM_PROJECT_ID}:${DOLLAR}REVISION_ID', '.']
+  - name: 'gcr.io/cloud-builders/docker'
+    args: ["push", "gcr.io/${DOLLAR}PROJECT_ID/${DOLLAR}{_DEXCOM_PROJECT_ID}:${DOLLAR}REVISION_ID"]
+  - name: 'gcr.io/cloud-builders/kubectl'
+    args:
+      - 'set'
+      - 'image'
+      - 'rs/${DOLLAR}{_DEXCOM_PROJECT_ID}-rs'
+      - 'spring-boot-container=gcr.io/${DOLLAR}PROJECT_ID/${DOLLAR}{_DEXCOM_PROJECT_ID}:${DOLLAR}REVISION_ID'
+      - '--namespace=${DOLLAR}{_DEXCOM_NAMESPACE}'
+    env:
+      - 'CLOUDSDK_COMPUTE_ZONE=${KUBE_REGION}'
+      - 'CLOUDSDK_CONTAINER_CLUSTER=${KUBE_CLUSTER}'
+EOF
+
+popd
 
